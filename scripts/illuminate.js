23e09d403202c32c473a9bc6aadf78ea28066a4c
MattWesterhaus <71035752+MattWesterhaus@users.noreply.github.com>
2022-04-28 15:23:36 -0500

    Update illuminate.js

--- scripts/illuminate.js
+++ scripts/illuminate.js
@@ -160,12 +160,6 @@ class GlApp {
         this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
         this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, image_element);
         this.gl.bindTexture(this.gl.TEXTURE_2D, null);
-        /*
-        this.gl.activeTexture(texture);
-        this.gl.bindTexture(this.gl.TEXTURE_2D, image_element);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);*/
     }
 
     render() {
